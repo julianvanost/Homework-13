4175078a917dc74c19f1b4343efa10ef223ae4d2
Julian <Van Ost>
2020-03-08 18:02:32 -0700

    attemp to get heroku db working after further configurations

--- config/connection.js
+++ config/connection.js
@@ -4,8 +4,7 @@ let connection;
 
 if (process.env.JAWSDB_URL) {
   connection = createConnection(process.env.JAWSDB_URL)
-}
-else {
+} else {
   connection = createConnection({
     host: 'bbj31ma8tye2kagi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     port: 3306,
--- views/components/formHamburger.jsx
+++ views/components/formHamburger.jsx
@@ -3,10 +3,10 @@ const React = require('react');
 function FormHamburger() {
   return (
     <form>
-      <div class="form-group">
-        <label for="inputHamburgerTextArea">Enter Burger Type</label>
-        <p><textarea class="form-control" id="inputHamburgerTextArea" rows="3"></textarea></p>
-        <p><button type="button" class="btn btn-primary btn-lg" id="submitHamburger">Add to Menu</button></p>
+      <div className="form-group">
+        <label htmlFor="inputHamburgerTextArea">Enter Burger Type</label>
+        <p><textarea className="form-control" id="inputHamburgerTextArea" rows="3"></textarea></p>
+        <p><button type="button" className="btn btn-primary btn-lg" id="submitHamburger">Add to Menu</button></p>
       </div>
     </form>
   );
